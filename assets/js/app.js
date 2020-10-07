e6fcac2b6fe9ca5191c0363fae4780a6f4b968e7
adsantiago <adsantiago.guacheta@gmail.com>
2020-10-06 22:47:27 -0500

    Primer commit del portafolio de servicios personal

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function changeClass(){
+    let menu = document.getElementById('menu');
+    let body = document.getElementById('all');
+        body.classList.toggle('difuse');
+        menu.classList.toggle('menu-open');
+}
+
+
